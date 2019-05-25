f1eeeddae7739fa9857293e0799eb82c6ffa7f34
Dennis Mo <dymo@edu.uwaterloo.ca>
2019-05-25 19:13:41 -0400

    overall layout and model

--- iSplit/ViewController.swift
+++ iSplit/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+//    lazy var scene = StartScene(size: view.bounds.size)
+    
+    @IBAction func addItem(_ sender: UIButton) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        let skView = view as! View
+//        skView.ignoresSiblingOrder = true
+//        skView.showsFPS = true
+//        skView.showsNodeCount = true
+//        scene.scaleMode = .resizeFill
+//        skView.presentScene(scene)
     }
-
-
+    
+    @IBAction func groups(_ sender: UIButton) {
+        
+    }
+    @IBOutlet weak var purchases: UITableView!
+    
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return false
+    }
 }
+
 
---
+++ iSplit/appBrain.swift
@@ -0,0 +1,48 @@
+//
+//  appBrain.swift
+//  iSplit
+//
+//  Created by Dennis Mo on 5/25/19.
+//  Copyright © 2019 Dennis Mo. All rights reserved.
+//
+
+import Foundation
+
+class user {
+    var name:String
+    var balance:Double
+    init(name:String){
+        self.name = name
+        self.balance = 0;
+    }
+}
+
+class group {
+    var users = [user]()
+    var tranHistory = [transactions]()
+    init(){
+        
+    }
+}
+
+
+struct transactions{
+    var tranName:String
+    var payTable = [String:Double]()
+    var date:Date
+    init(tranName:String,names:[String],personPaid:String,payRatio:[Double],totalAmount:Double,time:Date){
+        for name in names {
+            if (name == personPaid){
+                self.payTable[name] = totalAmount
+            }
+            else{
+                self.payTable[name] = 0
+            }
+        }
+        for i in names.indices {
+            self.payTable[names[i]]! -= payRatio[i] * totalAmount
+        }
+        self.date = time
+        self.tranName = tranName
+    }
+}
