18e2549026ce3260e92db75cd800ac5a6c28d696
Yifei Yin <yifeiyin@foxmail.com>
2019-08-29 13:52:09 +0800

    Lint appBrain.swift

--- iSplit/appBrain.swift
+++ iSplit/appBrain.swift
@@ -9,73 +9,70 @@
 import UIKit
 
 class user {
-    var name:String
-    var balance:Double
-    init(name:String){
+    var name: String
+    var balance: Double
+    init(name: String) {
         self.name = name
-        self.balance = 0;
+        self.balance = 0
     }
 }
 
 class group {
-    var groupName:String
-    var groupImage:UIImage?
+    var groupName: String
+    var groupImage: UIImage?
     var users = [user]()
     var tranHistory = [transaction]()
-    init(){
+    init() {
         groupName = ""
         users = []
     }
-    
-    init(name:String,members:[String]){
+
+    init(name: String, members: [String]) {
         groupName = name
         for u in members {
             users.append(user(name: u))
         }
     }
-    func getMembers()->String{
-        var temp:String = ""
+    func getMembers() -> String {
+        var temp = ""
         for u in users {
             temp.append(u.name)
             temp.append(" ")
         }
         return temp
     }
-    
+
 }
-struct transaction{
-    var tranName:String
+struct transaction {
+    var tranName: String
     var payTable = [Double]()
-    var date:Date
-    var totalAmount:Double
-    init(){
+    var date: Date
+    var totalAmount: Double
+    init() {
         tranName = ""
         date = Date()
         totalAmount = 0
     }
 }
 
-class centralBank:NSCoding{
-    
+class centralBank: NSCoding {
 
-    
-    var groups:[group]
-    var currGroup:group?
-    func newTransaction(trans:transaction){
+    var groups: [group]
+    var currGroup: group?
+    func newTransaction(trans: transaction) {
         currGroup?.tranHistory.insert(trans, at: 0)
-        for i in 0...currGroup!.users.count - 1{
+        for i in 0...currGroup!.users.count - 1 {
             currGroup!.users[i].balance += trans.payTable[i]*trans.totalAmount
         }
     }
-    init(){
+    init() {
         groups = []
     }
     func encode(with aCoder: NSCoder) {
-        
+
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         groups = []
     }
 }
-
