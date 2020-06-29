1691bc71ebd949edcae3f7d5cf329823919f4b43
Dennis Mo <dennismoyunfei@gmail.com>
2020-06-28 21:58:24 -0700

    Switch to FastAPI

--- Backend/services/backend-simple/s3_exp.py
+++
@@ -1,4 +0,0 @@
-import boto3
-s3 = boto3.client('s3')
-print(s3.list_objects_v2(Bucket='a204311-tr-corporate-nonprod-trlabs-usdev', MaxKeys=2))
-s3.upload_file('hello.txt', 'a204311-tr-corporate-nonprod-trlabs-usdev', 'data/test/hello.txt')
