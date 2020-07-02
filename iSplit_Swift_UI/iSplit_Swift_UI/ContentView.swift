0d0ca11e44375c9fcf4be63753153eb66251b472
Stellar Pan Jian Yu <50890075+StellarPan@users.noreply.github.com>
2020-07-01 23:05:56 -0400

    add Swift_UI
    add Swift_UI

---
+++ iSplit_Swift_UI/iSplit_Swift_UI/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  iSplit_Swift_UI
+//
+//  Created by Stellar Pan on 2020-07-01.
+//  Copyright © 2020 Stellar Pan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Button(action: {
+            // What to perform
+        }) {
+            // How the button looks like
+            Text("Hello, World!")
+        }
+    }
+}
+
+
+
+//Image(systemName: "plus.square")
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
